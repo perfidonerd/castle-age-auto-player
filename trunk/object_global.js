659e308b06bfc9492854750c9a9d960bc6350518
xotic750 <xotic750@e57812c5-01bb-cede-f156-02a94447bbef>
2010-08-16 21:19:23 +0000

    v140.23.51
    Stats: Work in progress

--- trunk/object_global.js
+++ trunk/object_global.js
@@ -3,26 +3,18 @@
 ///////////////////////////
 
 global = {
-    gameName: 'castle_age',
-
-    discussionURL: 'http://senses.ws/caap/index.php',
-
-    newVersionAvailable: false,
-
-    documentTitle: document.title,
-
-    is_chrome: navigator.userAgent.toLowerCase().indexOf('chrome') !== -1 ? true : false,
-
-    is_firefox: navigator.userAgent.toLowerCase().indexOf('firefox') !== -1  ? true : false,
-
+    gameName            : 'castle_age',
+    discussionURL       : 'http://senses.ws/caap/index.php',
+    newVersionAvailable : false,
+    documentTitle       : document.title,
+    is_chrome           : navigator.userAgent.toLowerCase().indexOf('chrome') !== -1 ? true : false,
+    is_firefox          : navigator.userAgent.toLowerCase().indexOf('firefox') !== -1  ? true : false,
     // Object separator - used to separate objects
-    os: '\n',
-
+    os                  : '\n',
     // Value separator - used to separate name/values within the objects
-    vs: '\t',
-
+    vs                  : '\t',
     // Label separator - used to separate the name from the value
-    ls: '\f',
+    ls                  : '\f',
 
     AddCSS: function () {
         try {
@@ -124,6 +116,40 @@ global = {
                 this.log(1, text);
             }
         }
+    },
+
+    ReloadCastleAge: function () {
+        // better than reload... no prompt on forms!
+        if (window.location.href.indexOf('castle_age') >= 0 && !gm.getValue('Disabled') && (gm.getValue('caapPause') === 'none')) {
+            if (global.is_chrome) {
+                CE_message("paused", null, gm.getValue('caapPause', 'none'));
+            }
+
+            window.location.href = "http://apps.facebook.com/castle_age/index.php?bm=1";
+        }
+    },
+
+    ReloadOccasionally: function () {
+        var reloadMin = gm.getNumber('ReloadFrequency', 8);
+        if (!reloadMin || reloadMin < 8) {
+            reloadMin = 8;
+        }
+
+        nHtml.setTimeout(function () {
+            if (caap.WhileSinceDidIt('clickedOnSomething', 5 * 60)) {
+                global.log(1, 'Reloading if not paused after inactivity');
+                if ((window.location.href.indexOf('castle_age') >= 0 || window.location.href.indexOf('reqs.php#confirm_46755028429_0') >= 0) &&
+                        !gm.getValue('Disabled') && (gm.getValue('caapPause') === 'none')) {
+                    if (global.is_chrome) {
+                        CE_message("paused", null, gm.getValue('caapPause', 'none'));
+                    }
+
+                    window.location.href = "http://apps.facebook.com/castle_age/index.php?bm=1";
+                }
+            }
+
+            global.ReloadOccasionally();
+        }, 1000 * 60 * reloadMin + (reloadMin * 60 * 1000 * Math.random()));
     },
 
     hashStr: [
